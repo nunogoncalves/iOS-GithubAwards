be08466bcb2d65d5962441705b5c0e369f945ebf
Nuno Gonçalves <nuno.goncalves@linkedcare.com>
2015-12-22 10:46:18 +0000

    Network requester refactor

--- GitHubAwards/Networking/NetworkRequester.swift
+++ GitHubAwards/Networking/NetworkRequester.swift
@@ -17,27 +17,42 @@ class NetworkRequester {
     }
     
     func makeGet(urlStr: String) {
-        let url = NSURL(string: urlStr.urlEncoded())
-        
         //http://stackoverflow.com/questions/24016142/how-to-make-an-http-request-in-swift
-        let task = NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: completionHandler)
+        let task = buildRequesterTaskFor(urlStr)
         task.resume()
     }
     
+    private func buildRequesterTaskFor(urlStr: String) -> NSURLSessionDataTask {
+        let url = NSURL(string: urlStr.urlEncoded())
+        return NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: completionHandler)
+    }
+    
     private func completionHandler(data: NSData?, response: NSURLResponse?, error: NSError?) {
-        if let response = response as? NSHTTPURLResponse {
-            if response.statusCode == 200 {
-                do {
-                    let data = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments) as? NSDictionary
-                    self.networkResponseHandler.success(data!)
-                } catch _ {
-                    self.networkResponseHandler.failure()
-                }
-            } else {
-                self.networkResponseHandler.failure()
-            }
-        } else {
-            self.networkResponseHandler.failure()
+        guard let response = response as? NSHTTPURLResponse else {
+            return handleFailure()
         }
+        
+        if response.statusCode == 200 {
+            handleSuccess(data)
+        } else {
+            handleFailure()
+        }
+    }
+    
+    private func handleSuccess(data: NSData?) {
+        do {
+            let data = try convertDataToDictionary(data!)
+            networkResponseHandler.success(data)
+        } catch _ {
+            handleFailure()
+        }
+    }
+    
+    private func handleFailure() {
+        networkResponseHandler.failure()
+    }
+    
+    private func convertDataToDictionary(data: NSData) throws -> NSDictionary {
+        return try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments) as! NSDictionary
     }
 }
