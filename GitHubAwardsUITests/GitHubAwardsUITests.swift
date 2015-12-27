9d3c9634f6c67384f852c9a2c00d96144b15d4f0
Nuno Gonçalves <nuno.goncalves@linkedcare.com>
2015-12-27 08:54:14 +0000

    Adding first ui tests... but being dependent on server data is not good...

--- GitHubAwardsUITests/GitHubAwardsUITests.swift
+++ GitHubAwardsUITests/GitHubAwardsUITests.swift
@@ -8,29 +8,37 @@
 
 import XCTest
 
-//class GitHubAwardsUITests: XCTestCase {
+class GitHubAwardsUITests: XCTestCase {
+    
+    let app = XCUIApplication()
+    
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        app.launch()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testLanguagesScreen() {
+        let languagesTitle = app.navigationBars.staticTexts["Languages"]
+        XCTAssertTrue(languagesTitle.exists, "Should be the title of the first screen")
+    }
+
+    func testListOfLanguages() {
+//        let app = XCUIApplication()
+//        let tabBarsQuery = app.tabBars
+//        tabBarsQuery.buttons["Contacts"].tap()
 //        
-//    override func setUp() {
-//        super.setUp()
-//        
-//        // Put setup code here. This method is called before the invocation of each test method in the class.
-//        
-//        // In UI tests it is usually best to stop immediately when a failure occurs.
-//        continueAfterFailure = false
-//        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-//        XCUIApplication().launch()
-//
-//        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-//    }
-//    
-//    override func tearDown() {
-//        // Put teardown code here. This method is called after the invocation of each test method in the class.
-//        super.tearDown()
-//    }
-//    
-//    func testExample() {
-//        // Use recording to get started writing UI tests.
-//        // Use XCTAssert and related functions to verify your tests produce the correct results.
-//    }
-//    
-//}
+//        let searchForAUserSearchField = app.searchFields["Search for a user"]
+//        searchForAUserSearchField.tap()
+//        searchForAUserSearchField.typeText("nun")
+//        app.buttons["Search"].tap()
+//        app.typeText("\n")
+//        tabBarsQuery.buttons["Featured"].tap()
+//        app.buttons["Try again"].tap()
+        
+    }
+}
