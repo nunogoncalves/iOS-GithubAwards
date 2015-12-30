8c137c64abb20f9856ddadd57fb2669cb9401cc1
Nuno Gonçalves <nuno.goncalves@linkedcare.com>
2015-12-30 14:48:03 +0000

    Browser class fix

---
+++ GitHubAwards/Use Cases/Browser.swift
@@ -0,0 +1,18 @@
+//
+//  Browser.swift
+//  GitHubAwards
+//
+//  Created by Nuno Gonçalves on 30/12/15.
+//  Copyright © 2015 Nuno Gonçalves. All rights reserved.
+//
+
+import UIKit
+
+class Browser {
+    
+    static func openPage(url: String) {
+        let url = NSURL(string: url)
+        UIApplication.sharedApplication().openURL(url!)
+    }
+    
+}
