9535ffbe6edbfbd01cbcbf3e4b023b9379efd993
Nuno Gonçalves <nuno.goncalves@linkedcare.com>
2016-03-27 23:15:03 +0100

    TwitterClient enum

--- OctoPodium/Use Cases/Follow.swift
+++ OctoPodium/Use Cases/Follow.swift
@@ -8,26 +8,33 @@
 
 struct Twitter {
 
+    enum TwitterClient : String {
+        case Twitter = "twitter://user?screen_name="
+        case TweetBot = "tweetbot://user_profile/"
+        case Echofon = "echofon://user_timeline?"
+        case TwittelatorPro = "twit://user?screen_name="
+        case Seesmic = "x-seesmic://twitter_profile?twitter_screen_name="
+        case Birdfeed = "x-birdfeed://user?screen_name="
+        case Tweetings = "tweetings://user?screen_name="
+        case SimplyTweet = "simplytweet://?link=http://twitter.com/"
+        case IceBird = "icebird://user?screen_name="
+        case Fluttr = "fluttr://user/"
+        
+        static func all() -> [TwitterClient] {
+            return [Twitter, TweetBot, Echofon, TwittelatorPro, Seesmic,
+                Birdfeed, Tweetings, SimplyTweet, IceBird, Fluttr]
+        }
+    }
+    
     struct Follow {
         
         init(username: String) {
-            let twitterURLs = [
-                "twitter://user?screen_name=\(username)", // Twitter
-                "tweetbot://user_profile/\(username)", // TweetBot
-                "echofon://user_timeline?\(username)", // Echofon
-                "twit://user?screen_name=\(username)", // Twittelator Pro
-                "x-seesmic://twitter_profile?twitter_screen_name=\(username)", // Seesmic
-                "x-birdfeed://user?screen_name=\(username)", // Birdfeed
-                "tweetings://user?screen_name=\(username)", // Tweetings
-                "simplytweet://?link=http://twitter.com/\(username)", // SimplyTweet
-                "icebird://user?screen_name=\(username)", // IceBird
-                "fluttr://user/\(username)", // Fluttr
-            ]
         
             var applicationOpened: Bool = false
             let application = UIApplication.sharedApplication()
-            for twitterURL in twitterURLs {
-                if let url = NSURL(string: twitterURL) where application.canOpenURL(url) && !applicationOpened {
+            for twitterClient in TwitterClient.all() {
+                let twitterUrl = "\(twitterClient.rawValue)\(username)"
+                if let url = NSURL(string: twitterUrl) where application.canOpenURL(url) && !applicationOpened {
                     application.openURL(url)
                     applicationOpened = true
                     break
