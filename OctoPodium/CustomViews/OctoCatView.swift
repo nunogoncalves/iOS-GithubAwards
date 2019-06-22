f5e8ffd06bf5a65c3aa3edcad5be4ff3d1978d70
Nuno Gonçalves <numicago@gmail.com>
2019-06-22 18:57:17 +0100

    Adds first SwiftUI view - OctoCatView

---
+++ OctoPodium/CustomViews/OctoCatView.swift
@@ -0,0 +1,56 @@
+//
+//  SwiftUIView.swift
+//  OctoPodium
+//
+//  Created by Nuno Gonçalves on 20/06/2019.
+//  Copyright © 2019 Nuno Gonçalves. All rights reserved.
+//
+
+import SwiftUI
+
+struct OctoCatView : View {
+
+    @State var eyesOpened: Bool = false
+
+    var body: some View {
+        ZStack {
+            Image(uiImage: #imageLiteral(resourceName: "OctoCat"))
+                .resizable()
+                .frame(width: 200, height: 200, alignment: .center)
+            HStack(alignment: .center, spacing: 40) {
+                Eye(opened: eyesOpened)
+                Eye(opened: eyesOpened)
+            }
+            .padding(EdgeInsets(top: 90, leading: 0, bottom: 0, trailing: 0))
+        }
+    }
+}
+
+private struct Eye : View {
+
+    @State var opened: Bool = false
+
+    var body: some View {
+        Image(uiImage: opened ? #imageLiteral(resourceName: "EyeBall") : #imageLiteral(resourceName: "X"))
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: eyeWidth, height: eyeHeight, alignment: .center)
+    }
+
+    private var eyeWidth: CGFloat { opened ? 34 : 24 }
+    private var eyeHeight: CGFloat { opened ? 50 : 25 }
+}
+
+fileprivate extension CGFloat {
+}
+#if DEBUG
+struct SwiftUIView_Previews : PreviewProvider {
+    static var previews: some View {
+        Group {
+            OctoCatView(eyesOpened: true)
+            OctoCatView(eyesOpened: false)
+        }
+        .previewLayout(.sizeThatFits)
+    }
+}
+#endif
