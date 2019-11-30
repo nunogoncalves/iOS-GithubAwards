1892455d19e60d6e9e01f14a36438a1cf28b3c37
Nuno Gonçalves <numicago@gmail.com>
2019-11-30 23:22:13 +0000

    Removes Kanna in favour of SwiftSoupe

--- OctoPodium/Use Cases/Repositories/CreateListFromHTML.swift
+++ OctoPodium/Use Cases/Repositories/CreateListFromHTML.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Nuno Gonçalves. All rights reserved.
 //
 
-import Kanna
+import SwiftSoup
 
 extension Repositories {
 
@@ -14,50 +14,27 @@ extension Repositories {
         
         static func repositories(containedIn html: String) -> [Repository] {
 
-            guard let document = try? Kanna.HTML(html: html, encoding: .utf8) else { return [] }
-            
-            return repositoriesHTML(document).compactMap { repository(from: $0) }
-        }
+            guard let soup = try? SwiftSoup.parse(html),
+                let repoHtmls = try? soup.body()?.getElementsByClass("Box-row")
+            else { return [] }
 
-        private static func repositoriesHTML(_ document: HTMLDocument) -> XPathObject {
-            return document.xpath("//*[contains(@class, 'Box-row')]")
-        }
+            return repoHtmls.compactMap { repo -> GitAwardsRepository? in
 
-        private static func repository(from repoHTML: XMLElement) -> Repository? {
-            guard let repoName = repositoryName(from: repoHTML), !repoName.isEmpty else { return nil }
-
-            let starsCount = stars(from: repoHTML)
-            let descr = description(from: repoHTML)
-            let lang = language(from: repoHTML)
-
-            return GitAwardsRepository(name: repoName, stars: starsCount, description: descr, language: lang)
-        }
-
-        private static func repositoryName(from document: XMLElement) -> String? {
-            return document.css(".lh-condensed").makeIterator().next()?.text?.withoutSpaces()
-        }
-        
-        private static func stars(from document: XMLElement) -> String {
-            let total = document.css("a.mr-3").makeIterator().next()?.text?.withoutSpaces() ?? ""
-
-            let stars = document.css("span.float-sm-right")
-                                   .makeIterator().next()?.text?
-                                   .withoutSpaces().substringUntil("stars") ?? ""
-            
-            return "\(total) (\(stars))"
-        }
-        
-        private static func description(from document: XMLElement) -> String {
-            guard let txt =  document.css("p.my-1").makeIterator().next()?.text else { return "" }
-            
-            return txt.trimmingCharacters(in: .whitespaces).replace("\n", with: "").replace("      ", with: "")
-        }
-        
-        private static func language(from document: XMLElement) -> String? {
-            if let element = document.css("span.mr-3").makeIterator().next() {
-                return element.text?.withoutSpaces()
+                guard let name = try? repo.select(".lh-condensed").text().withoutSpaces(),
+                    let stars = try? repo.select("a.mr-3").first()?.text(),
+                    let language = try? repo.select("span.mr-3").first()?.text(),
+                    let description = try? repo.select("p.my-1").first()?.text(),
+                    let starsOfTime = try? repo.select("span.float-sm-right").text().substringUntil(" stars")
+                else {
+                    return nil
+                }
+                return GitAwardsRepository(
+                    name: name,
+                    stars: "\(stars) (\(starsOfTime))",
+                    description: description,
+                    language: language
+                )
             }
-            return nil
         }
     }
 }
--- Podfile
+++ Podfile
@@ -3,9 +3,6 @@ platform :ios, '10.0'
 
 target 'OctoPodium' do
 
-#    pod 'Kanna', '~> 1.0.6'
-    pod 'Kanna', :git => 'https://github.com/tid-kijyun/Kanna.git', branch: 'master'
-#    pod 'Locksmith', '2.0.8'
     pod 'Locksmith', '4.0.0'
     pod 'ARSPopover', '~>2.2.1'
     pod 'OHHTTPStubs/Swift', '~>6.1.0'
@@ -13,8 +10,6 @@ target 'OctoPodium' do
     pod 'netfox', '~>1.15.0', configurations: ['debug']
 
     target 'OctoPodiumTests' do
-      pod 'Quick', '2.1'
-      pod 'Nimble', '~>8.0.1'
       pod 'iOSSnapshotTestCase'
     end
 end
